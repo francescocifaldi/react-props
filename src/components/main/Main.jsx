f8f95bd49b3988e24d00eeb44d560bba87473b99
francescocifaldi <francesco.cifaldi@gmail.com>
2024-12-02 23:39:51 +0100

    ADD unique tags print

--- src/components/main/Main.jsx
+++ src/components/main/Main.jsx
@@ -2,8 +2,24 @@ import Card from "../card/Card";
 import posts from "../../posts";
 
 export default function Main() {
+    const uniqueTags = []
+    posts.forEach((post) => {
+        post.tags.forEach((tag) => {
+            if (!uniqueTags.includes(tag)) {
+                uniqueTags.push(tag)
+            }
+        })
+    })
+
     return (
         <>
+
+            <ul>
+                {uniqueTags.map((uniqueTag) => (
+                    <li key={uniqueTag}>{uniqueTag}</li>
+                ))}
+            </ul>
+
             {
                 posts.map((post) => (
                     < div key={post.id} className="container" >
